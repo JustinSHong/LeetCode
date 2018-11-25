11411e1b318ae120ad86f2327421d69071c6565d
JustinSHong <jhong2009@gmail.com>
2018-11-24 21:34:59 -0800

    longestSubstr.js: added task description and test cases

---
+++ JavaScript/longestSubstr.js
@@ -0,0 +1,7 @@
+// Task Description
+//Given a string, find the length of the longest substring without repeating characters.
+
+// Test Cases
+lengthOfLongestSubstring("abcabcbb"); // 3
+lengthOfLongestSubstring("bbbbb"); // 1
+lengthOfLongestSubstring("pwwkew"); // 3
