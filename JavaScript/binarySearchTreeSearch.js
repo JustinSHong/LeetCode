a081381ba6eacb47cbcab56300cb3a737b852597
JustinSHong <jhong2009@gmail.com>
2018-11-24 22:23:01 -0800

    binarySearchTreeSearch.js: added solution using iteration

--- JavaScript/binarySearchTreeSearch.js
+++ JavaScript/binarySearchTreeSearch.js
@@ -3,6 +3,21 @@
 // You need to find the node in the BST that the node's value equals the given value.
 // Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
 
+// Method: Iteration
+var searchBST = function(root, val) {
+    let current = root;
+    while (current) {
+        if (current.val === val) return current;
+        else if (current.val < val) {
+            if (current.right) current = current.right;
+        } else if (current.val > val) {
+            if (current.left) current = current.left;
+        }
+    }
+    return null;
+};
+
+// Method: Recursion
 var searchBST = function(root, val) {
     // root has the target val
     if (root.val === val) {
