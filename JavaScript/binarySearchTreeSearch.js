5c8c2a947efbc45d714fa49df9c8c3a2dab39cc1
JustinSHong <jhong2009@gmail.com>
2018-11-24 22:19:28 -0800

    binarySearchTreeSearch.js: added task description and test case

---
+++ JavaScript/binarySearchTreeSearch.js
@@ -0,0 +1,7 @@
+// Task Description
+// Given the root node of a binary search tree (BST) and a value.
+// You need to find the node in the BST that the node's value equals the given value.
+// Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+// Test Cases
+searchBST([4, 2, 7, 1, 3], 2);
