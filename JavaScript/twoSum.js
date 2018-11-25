9319c678c46917d0c09dd6202b040e3a67c28d89
JustinSHong <jhong2009@gmail.com>
2018-11-24 21:22:52 -0800

    twoSum.js: added solution using hash

--- JavaScript/twoSum.js
+++ JavaScript/twoSum.js
@@ -7,5 +7,15 @@
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
 
+var twoSum = function(nums, target) {
+    const hash = {};
+    // populate hash
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (hash.hasOwnProperty(complement)) return [i, hash[complement]];
+        hash[nums[i]] = i;
+    }
+};
+
 // Test Cases
-twoSum([2, 7, 11, 15]); // [1, 0]
+twoSum([2, 7, 11, 15], 9); // [1, 0]
