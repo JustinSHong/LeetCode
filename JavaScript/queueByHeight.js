0d6ca3790dcb54e4f8874d7edcb23fd1f5a95542
JustinSHong <jhong2009@gmail.com>
2018-11-24 22:08:51 -0800

    queueByHeight.js: added initial solution

--- JavaScript/queueByHeight.js
+++ JavaScript/queueByHeight.js
@@ -4,5 +4,19 @@
 // where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h.
 // Write an algorithm to reconstruct the queue.
 
+var reconstructQueue = function(people) {
+    const result = [];
+    people = people.sort((a, b) => {
+        if (a[0] == b[0]) return a[1] - b[1];
+
+        return b[0] - a[0];
+    });
+
+    for (let i = 0; i < people.length; i++) {
+        result.splice(people[i][1], 0, people[i]);
+    }
+    return result;
+};
+
 // Test Cases
 reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]); // [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
