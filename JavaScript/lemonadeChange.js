72b9a6061e7eb2be4eac906adb97775fd2b01833
JustinSHong <jhong2009@gmail.com>
2018-11-24 21:59:17 -0800

    lemonadeChange.js: added initial solution

--- JavaScript/lemonadeChange.js
+++ JavaScript/lemonadeChange.js
@@ -7,6 +7,29 @@
 // Note that you don't have any change in hand at first.
 // Return true if and only if you can provide every customer with correct change.
 
+var lemonadeChange = function(bills) {
+    const register = {};
+
+    for (let bill of bills) {
+        if (!register.hasOwnProperty(bill)) register[bill] = 0;
+        register[bill]++;
+        if (bill == 10) {
+            // customer needs $5 in change
+            if (register[5] > 0) register[5]--;
+            else return false;
+        } else if (bill == 20) {
+            // customer needs three $5 or one $5 and $10
+            if (register[5] >= 1 && register[10] >= 1) {
+                register[5]--;
+                register[10]--;
+            } else if (register[5] >= 3) {
+                register[5] -= 3;
+            } else return false;
+        }
+    }
+    return true;
+};
+
 // Test Cases
 lemonadeChange([5, 5, 5, 10, 20]); // true
 lemonadeChange([5, 5, 10]); // true
