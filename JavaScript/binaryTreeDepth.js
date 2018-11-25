c4530b55b41164962456b706006099c2f36ea56a
JustinSHong <jhong2009@gmail.com>
2018-11-24 22:27:08 -0800

    binaryTreeDepth.js: added solution using iteration

--- JavaScript/binaryTreeDepth.js
+++ JavaScript/binaryTreeDepth.js
@@ -3,5 +3,33 @@
 // The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 // Note: A leaf is a node with no children.
 
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+var maxDepth = function(root) {
+    const stack = [];
+    let depth = 0;
+
+    // push the root to the stack
+    if (root) {
+        stack.push([1, root]);
+    }
+
+    while (stack.length > 0) {
+        // deconstruct depth and current node
+        let [currentDepth, current] = stack.pop();
+
+        if (current) {
+            depth = Math.max(depth, currentDepth);
+            // increment currentDepth at each depth
+            stack.push([currentDepth + 1, current.left]);
+            stack.push([currentDepth + 1, current.right]);
+        }
+    }
+    return depth;
+};
+
 // Test Cases
 // given [3,9,20,null,null,15,7] tree return depth of 3
