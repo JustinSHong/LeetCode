d155dd08b2bd38267844a3c81584a99f8169311d
JustinSHong <jhong2009@gmail.com>
2018-11-24 21:42:43 -0800

    minStack.js: added initial solution

--- JavaScript/minStack.js
+++ JavaScript/minStack.js
@@ -5,3 +5,48 @@
 // pop() -- Removes the element on top of the stack.
 // top() -- Get the top element.
 // getMin() -- Retrieve the minimum element in the stack.
+
+var MinStack = function() {
+    this.storage = [];
+    this.min = []; // holds smallest values of the main stack
+    this.size = 0;
+};
+
+MinStack.prototype.push = function(x) {
+    if (this.size == 0) {
+        this.storage.push(x);
+        this.min.push(x);
+        this.size++;
+    } else {
+        if (x > this.min[this.size - 1]) {
+            this.min.push(this.min[this.size - 1]);
+        } else {
+            this.min.push(x);
+        }
+        this.storage.push(x);
+        this.size++;
+    }
+};
+
+MinStack.prototype.pop = function() {
+    this.storage.pop();
+    this.min.pop();
+    this.size--;
+};
+
+MinStack.prototype.top = function() {
+    return this.storage[this.size - 1];
+};
+
+MinStack.prototype.getMin = function() {
+    return this.min[this.size - 1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = Object.create(MinStack).createNew()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
