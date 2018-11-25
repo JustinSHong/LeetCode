6998c034301b6665f6479b04c6e3e820b42f3941
JustinSHong <jhong2009@gmail.com>
2018-11-24 21:40:26 -0800

    add initial solution

--- JavaScript/longestSubstr.js
+++ JavaScript/longestSubstr.js
@@ -1,5 +1,27 @@
 // Task Description
-//Given a string, find the length of the longest substring without repeating characters.
+// Given a string, find the length of the longest substring without repeating characters.
+
+var lengthOfLongestSubstring = function(s) {
+    let len = s.length;
+    let i = 0,
+        j = 0,
+        maxLen = 0,
+        substr = "";
+
+    while (i < len && j < len) {
+        // slide the window by 1
+        if (!substr.includes(s[j])) {
+            substr += s[j];
+            maxLen = Math.max(maxLen, substr.length);
+            j++;
+        } else {
+            substr = "";
+            i++;
+            j = i;
+        }
+    }
+    return maxLen;
+};
 
 // Test Cases
 lengthOfLongestSubstring("abcabcbb"); // 3
