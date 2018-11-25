380574ae6db5ecdb107e9ef595e73ddc54f00a8e
JustinSHong <jhong2009@gmail.com>
2018-11-24 21:44:36 -0800

    stackUsingQueues.js: added task description

---
+++ stackUsingQueues.js
@@ -0,0 +1,8 @@
+// Task Description
+
+// Implement the following operations of a stack using queues.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// empty() -- Return whether the stack is empty.
