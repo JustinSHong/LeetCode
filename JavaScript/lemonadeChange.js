69a80d21b2d486cd64af1eefdf13fa816007f06d
JustinSHong <jhong2009@gmail.com>
2018-11-24 21:56:46 -0800

    lemonadChange.js: added task description and test cases

---
+++ JavaScript/lemonadeChange.js
@@ -0,0 +1,13 @@
+// Task Description
+// At a lemonade stand, each lemonade costs $5.
+// Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+// Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.
+// You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+
+// Note that you don't have any change in hand at first.
+// Return true if and only if you can provide every customer with correct change.
+
+// Test Cases
+lemonadeChange([5, 5, 5, 10, 20]); // true
+lemonadeChange([5, 5, 10]); // true
+lemonadeChange([10, 10]); // true
